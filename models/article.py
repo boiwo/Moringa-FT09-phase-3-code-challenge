2927d51f6a2c95b10de05b67cafde9214437e753
benard <benardboiwo703@gmail.com>
2024-06-10 21:40:47 +0300

    initial commit

--- models/article.py
+++ models/article.py
@@ -1,10 +1,33 @@
+from database.connection import get_db_connection
+from models.author import Author
+from models.magazine import Magazine
+
 class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
+    def __init__(self, id, title, author, magazine):
+        self._id = id
         self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+        self.author = author
+        self.magazine = magazine
+
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Id must be an integer.")
+        self._id = value
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 50):
+            raise ValueError("Title must be a string between 1 and 50 characters.")
+        self._title = value
 
     def __repr__(self):
-        return f'<Article {self.title}>'
+        return f"Article(title='{self.title}', author={self.author}, magazine={self.magazine})"
