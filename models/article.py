c156f2d28d1f176e7769afe3e814bebdfa9d4b08
benard <benardboiwo703@gmail.com>
2024-06-10 22:08:43 +0300

    initial commit

--- models/article.py
+++ models/article.py
@@ -1,33 +1,65 @@
-from database.connection import get_db_connection
-from models.author import Author
-from models.magazine import Magazine
-
 class Article:
-    def __init__(self, id, title, author, magazine):
-        self._id = id
+    def __init__(self,id=None, title=None, content=None, author=None, magazine=None,conn = None,author_id=None,magazine_id=None):
+        self.id = id
         self.title = title
-        self.author = author
-        self.magazine = magazine
+        self.content = content
+        self.author_id = author_id if author_id else author.id
+        self.magazine_id = magazine_id if magazine_id else magazine.id
+        self.conn = conn
 
-    @property
-    def id(self):
-        return self._id
+        if conn:
+            self.cursor = self.conn.cursor()
+            self.add_to_database()
+
+    def __repr__(self):
+        return f'<Article {self.title}>'
     
-    @id.setter
-    def id(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Id must be an integer.")
-        self._id = value
+
+    def add_to_database(self):
+        sql = "INSERT INTO articles (title,content,author_id,magazine_id) VALUES (?,?,?,?)"
+        self.cursor.execute(sql,(self.title,self.content,self.author_id,self.magazine_id))
+        self.conn.commit()
+        self.id = self.cursor.lastrowid
 
     @property
     def title(self):
+        if not hasattr(self,"_title"):
+            sql = "SELECT title FROM articles WHERE id = ?"
+            row = self.cursor.execute(sql,(self.id,)).fetchone()
+            if row:
+                self._title = row[0]
         return self._title
-
+        
     @title.setter
-    def title(self, value):
-        if not isinstance(value, str) or not (1 <= len(value) <= 50):
-            raise ValueError("Title must be a string between 1 and 50 characters.")
-        self._title = value
+    def title(self,title):
+        if isinstance(title,str) and 5 <= len(title) <= 50 and not hasattr(self,"_title") :
+            self._title = title
+        else:
+            raise ValueError("Title must be a string between 5 and 50 characters long and can only be set once.")
 
-    def __repr__(self):
-        return f"Article(title='{self.title}', author={self.author}, magazine={self.magazine})"
+    def author(self):
+        from models.author import Author
+        sql = "SELECT authors.* FROM articles INNER JOIN authors ON articles.author_id = authors.id WHERE articles.id = ?"
+        row = self.cursor.execute(sql,(self.id,)).fetchone()
+        if row:
+            return Author(id=row[0], name=row[1], conn=self.conn)
+        else:
+            return None
+
+    
+    def magazine(self):
+        from models.magazine import Magazine
+        sql = "SELECT magazines.* FROM articles INNER JOIN magazines ON articles.magazine_id = magazines.id WHERE articles.id = ?"
+        row = self.cursor.execute(sql,(self.id,)).fetchone()
+        if row:
+            return Magazine(id=row[0],name=row[1],category=row[2],conn=self.conn)
+        else:
+            return None
+
+
+    @classmethod
+    def get_all_articles(cls,conn):
+        sql = "SELECT * FROM articles"
+        cursor = conn.cursor()
+        articles = cursor.execute(sql).fetchall()
+        return [cls(id=row[0],title=row[1],content=row[2],author_id=row[3],magazine_id=row[4],conn=conn) for row in articles]
--- models/author.py
+++ models/author.py
@@ -1,52 +1,83 @@
-from database.connection import get_db_connection
 
 class Author:
-    def __init__(self, id, name):
-        self._id = id
+    def __init__(self,id=None, name=None, conn = None):
         self.name = name
+        self.conn = conn
+        self.id = id
+      
+        if conn:
+            self.cursor = conn.cursor()
+            self.add_author()
 
+    def __repr__(self):
+        return f'<Author {self.name}>'
+    
+    def __eq__(self, other):
+        if isinstance(other, Author):
+            return self.id == other.id and self.name == other.name
+        return False
+
+    def __hash__(self):
+        return hash((self.id, self.name))
+    
+    def add_author(self):
+        sql_check = "SELECT id FROM authors WHERE name = ? LIMIT 1"
+        result = self.cursor.execute(sql_check,(self.name,)).fetchone()
+        if result:
+            self._id = result[0]
+        else:
+            sql = "INSERT INTO authors(name) VALUES (?)"
+            self.cursor.execute(sql, (self.name,))
+            self.conn.commit()
+            self._id = self.cursor.lastrowid
+    
     @property
     def id(self):
         return self._id
     
     @id.setter
     def id(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Id must be an integer.")
-        self._id = value
+        if value is None:
+            self._id = 0  # Set a default value or handle None case appropriately
+        elif isinstance(value, int):
+            self._id = value
+        else:
+            raise ValueError("Invalid ID value")
+
 
     @property
-    def name(self):
+    def name(self): 
+        if not hasattr(self,"_name"):
+            sql = "SELECT name FROM author WHERE id = ?"
+            self.cursor.execute(sql,(self.id,))
+            row = self.cursor.fetchone()
+            if row:
+                self._name = row[0]
         return self._name
-
+    
     @name.setter
-    def name(self, value):
-        if not isinstance(value, str) or len(value) == 0:
-            raise ValueError("Name must be a non-empty string.")
-        self._name = value
-
+    def name(self,name):
+        if isinstance(name,str) and len(name) and not hasattr(self,"_name"):
+            self._name = name
+        
+        else:
+            raise ValueError("Invalid name value")
+        
     def articles(self):
         from models.article import Article
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM articles WHERE author_id = ?', (self.id,))
-        rows = cursor.fetchall()
-        conn.close()
-        return [Article(row['id'], row['title']) for row in rows]
-
+        sql = "SELECT * FROM articles WHERE author_id = ?"
+        rows = self.cursor.execute(sql,(self.id,)).fetchall()
+        return [Article(id=row[0],title=row[1],content=row[2],author_id=row[3],magazine_id=row[4],conn=self.conn) for row in rows]
+    
     def magazines(self):
-        from models.magazine import Magazine 
-        conn = get_db_connection()
+        from models.magazine import Magazine
+        sql = """SELECT DISTINCT magazines.id, magazines.name, magazines.category FROM magazines INNER JOIN articles ON articles.magazine_id = magazines.id WHERE articles.author_id = ?"""
+        rows = self.cursor.execute(sql,(self.id,)).fetchall()
+        return [Magazine(id=row[0],name=row[1],category=row[2],conn=self.conn) for row in rows]
+
+    @classmethod
+    def get_all_authors(cls,conn):
+        sql = "SELECT * FROM authors"
         cursor = conn.cursor()
-        cursor.execute('''
-            SELECT DISTINCT magazines.* FROM magazines
-            JOIN articles ON articles.magazine_id = magazines.id
-            WHERE articles.author_id = ?
-        ''', (self.id,))
-        rows = cursor.fetchall()
-        conn.close()
-        return [Magazine(row['id'], row['name'], row['category']) for row in rows]
-
-    def __repr__(self):
-        return f'<Author {self.name}>'
-
+        authors = cursor.execute(sql).fetchall()
+        return [cls(name=author[1],conn=conn) for author in authors]
